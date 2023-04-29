d191f453b1e0ecd2774d12ad8e5dc27b6bdcc4c3
ginofrancocba <ginofrancocba@gmail.com>
2023-04-29 11:43:50 -0300

    Primer commit creo repo

---
+++ archivo1.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+/*<ul class="first">
+    <li>
+        <img src="" alt="">
+    </li>;
+    <li>
+        <img src="" alt="">
+    </li>;
+    <li>
+        <img src="" alt="">
+    </li>;
+
+</ul> */
